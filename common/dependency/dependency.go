8c8a09e4ecc59fb98c3efcb6c97ee65810d0c7d0
Ryan Martin <rmrt1n@users.noreply.github.com>
2024-02-17 00:21:29 +0700

    feat: switch to podman if no docker

--- common/dependency/dependency.go
+++ common/dependency/dependency.go
@@ -7,6 +7,7 @@ import (
 )
 
 var (
+	cmd = containerCmd()
 	Git = Dependency{
 		Name: "Git",
 		Cmd:  exec.Command("git", "--version"),
@@ -21,19 +22,19 @@ Learn how to install Go: https://go.dev/doc/install`,
 	}
 	Docker = Dependency{
 		Name: "Docker",
-		Cmd:  exec.Command("docker", "--version"),
+		Cmd:  exec.Command(cmd, "--version"),
 		Help: `Docker is required to build and run World Engine game shards.
 Learn how to install Docker: https://docs.docker.com/engine/install/`,
 	}
 	DockerCompose = Dependency{
 		Name: "Docker Compose",
-		Cmd:  exec.Command("docker", "compose", "version"),
+		Cmd:  exec.Command(cmd, "compose", "version"),
 		Help: `Docker Compose is required to build and run World Engine game shards.
 Learn how to install Docker: https://docs.docker.com/engine/install/`,
 	}
 	DockerDaemon = Dependency{
 		Name: "Docker daemon is running",
-		Cmd:  exec.Command("docker", "info"),
+		Cmd:  exec.Command(cmd, "info"),
 		Help: `Docker daemon needs to be running.
 If you use Docker Desktop, make sure that you have ran it`,
 	}
@@ -48,6 +49,15 @@ type Dependency struct {
 	Name string
 	Cmd  *exec.Cmd
 	Help string
+}
+
+func containerCmd() string {
+	cmd := exec.Command("docker", "-v")
+	err := cmd.Run()
+	if err != nil {
+		return "podman"
+	}
+	return "docker"
 }
 
 func (d Dependency) Check() error {
